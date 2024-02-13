e5b2f6c0aa48a7899f6205a5462bb7261104cea2
dali jerbi <dali.jerbi97@gmail.com>
2024-02-13 03:15:20 +0100

    feat(main.ts): added shutdown hooks and api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from '@/config/config.type';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableShutdownHooks();
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: ['/'],
+    },
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  await app.listen(
+    configService.getOrThrow('app.port', {
+      infer: true,
+    }),
+  );
 }
-bootstrap();
+void bootstrap();
