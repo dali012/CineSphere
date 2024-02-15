e005da8ec827bc9690cbdf4c29bbdaa6a1169f3d
dali jerbi <dali.jerbi97@gmail.com>
2024-02-15 05:20:59 +0100

    feat(movies.service): added default sort by year desc and search by ear

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -23,6 +23,58 @@ export class MoviesService {
         include: {
           torrents: true,
         },
+        orderBy: {
+          year: 'desc',
+        },
+      },
+      {
+        page,
+        perPage,
+      },
+    );
+  }
+
+  /**
+   * Find a movie by its unique identifier.
+   *
+   * @param {number} id - The unique identifier of the movie.
+   * @return {Promise<Movie>} The movie found by the unique identifier.
+   */
+  async findOne(id: number) {
+    try {
+      const movie = await this.prisma.movie.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          torrents: true,
+        },
+      });
+      return movie;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+   * Retrieves movies from a specific year with pagination.
+   *
+   * @param {number} year - the year of the movies to retrieve
+   * @param {number} page - the page number for pagination
+   * @param {number} perPage - the number of items per page for pagination
+   * @return {Promise<Promise<PaginatedResult<MovieEntity>>>} a promise that resolves to an array of MovieEntity objects
+   */
+  async getByYear(year: number, page: number, perPage: number) {
+    const paginate = createPaginator({ page, perPage });
+    return await paginate<MovieEntity, Prisma.MovieFindManyArgs>(
+      this.prisma.movie,
+      {
+        where: {
+          year,
+        },
+        include: {
+          torrents: true,
+        },
       },
       {
         page,
